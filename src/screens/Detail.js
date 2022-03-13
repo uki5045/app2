5b015cb9d5388feb96667809927112f4a890b5d9
hanjinwook <uki5045@gmail.com>
2022-03-13 16:35:41 +0900

    ? :

--- src/screens/Detail.js
+++ src/screens/Detail.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import {useParams, useLocation} from "react-router-dom";
 
 const Detail = () => {
+
+    const params = useParams()
+    const location = useLocation()
+
+    console.log(location)
+
+    const movie = location.pathname.includes('movie')
+    
+
+
     return (
         <div>
-            <h1>디테일</h1>
+            <h1>
+            {
+                movie
+                    ? params.movieId
+                    : params.tvId
+            }
+            </h1>
+            {/*<h1>{params.movieId}</h1>*/}
         </div>
     );
 };
