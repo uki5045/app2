deb3cc11146fe7cf859a88c2abd8c86c66fafecc
hanjinwook <uki5045@gmail.com>
2022-03-13 16:51:20 +0900

    Detail(networking with tv movie)

--- src/screens/Detail.js
+++ src/screens/Detail.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams, useLocation} from "react-router-dom";
+import axios from "axios";
+
 
 const Detail = () => {
 
     const params = useParams()
     const location = useLocation()
 
-    console.log(location)
+    const [item, setItem] = useState({})
 
-    const movie = location.pathname.includes('movie')
-    
+    const getItem = async () => {
+
+        try {
+           const {data, status} = location.pathname.includes('movie')
+                ? await axios.get(`https://api.themoviedb.org/3/movie/${params.movieId}?api_key=8597e491ed6e80f0de12e349eb60ea6e&language=en-US`)
+                : await axios.get(`https://api.themoviedb.org/3/tv/${params.tvId}?api_key=8597e491ed6e80f0de12e349eb60ea6e&language=en-US`)
+            if (status === 200) {
+                setItem(data)
+            }
+        } catch (err) {
+            console.log(err.response.data.message)
+        }
+    }
+
+    useEffect(() => {
+        getItem()
+    }, [])
+
+    // const movie = location.pathname.includes('movie')
+
 
 
     return (
         <div>
             <h1>
-            {
-                movie
-                    ? params.movieId
-                    : params.tvId
-            }
+                {item.title
+                    ? item.title
+                    : item.name
+                }
             </h1>
             {/*<h1>{params.movieId}</h1>*/}
         </div>
