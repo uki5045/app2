0e22a13961dc78c795a7cecec8d439e4ab4712f5
hanjinwook <uki5045@gmail.com>
2022-03-13 14:40:27 +0900

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {Home, Profile, Order, Payment} from "./screens";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    return (
+       <BrowserRouter>
+           <Routes>
+               <Route path={"/"} element={<Home />} />
+               <Route path={"/order"} element={<Order />}/>
+               <Route path={"/profile"} element={<Profile />}/>
+               <Route path={"/payment"} element={<Payment />}/>
+           </Routes>
+       </BrowserRouter>
+    );
+};
 
 export default App;
---
+++ src/screens/index.js
@@ -0,0 +1,6 @@
+import Home from "./Home";
+import Profile from "./Profile";
+import Order from "./Order";
+import Payment from "./Payment";
+
+export {Home, Profile, Order, Payment}
